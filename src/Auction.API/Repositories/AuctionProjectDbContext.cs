94f379f4579d05d8c799d27f7f5cacbf55482457
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-06 23:54:53 -0300

    feat(AuctionProjectDbContext.cs) adicionando conexão com o banco de dados SQLite

---
+++ src/Auction.API/Repositories/AuctionProjectDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AuctionProject.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionProject.API.Repositories;
+
+public class AuctionProjectDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=C:\Users\guied\OneDrive\Área de Trabalho\Estudos Programação\NLW-C#\leilaoDbNLW.db");
+    }
+}
