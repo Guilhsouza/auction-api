134e47c1b4180edb1c4216e4bb28bd389abc2baa
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-06 22:35:47 -0300

    feat(src/Auction.API) adicionando configuração inicial

---
+++ src/Auction.API/Controllers/AuctionController.cs
@@ -0,0 +1,20 @@
+﻿using AuctionProject.API.useCases.Auctions.GetCurrent;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionProject.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAuctionResult()
+    {
+        var useCase = new GetCurrentAuction();
+
+        var result = useCase.execute();
+
+        return Ok(result);
+    }
+}
+
---
+++ src/Auction.API/useCases/Auctions/GetCurrent/GetCurrentAuction.cs
@@ -0,0 +1,14 @@
+﻿using AuctionProject.API.Entities;
+
+namespace AuctionProject.API.useCases.Auctions.GetCurrent;
+
+public class GetCurrentAuction
+{
+    public Auction execute()
+    {
+        return new Auction
+        {
+           
+        };
+    }
+}
