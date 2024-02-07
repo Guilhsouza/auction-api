d939834f858d2f415ea66bdbf396d7d0859c44da
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-06 23:58:02 -0300

    feat(GetCurrentAuction.cs) adicionando retorno de leilão apenas para os que estão abertos na data atual

--- src/Auction.API/useCases/Auctions/GetCurrent/GetCurrentAuction.cs
+++ src/Auction.API/useCases/Auctions/GetCurrent/GetCurrentAuction.cs
@@ -1,14 +1,20 @@
 ﻿using AuctionProject.API.Entities;
+using AuctionProject.API.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuctionProject.API.useCases.Auctions.GetCurrent;
 
 public class GetCurrentAuction
 {
-    public Auction execute()
+    public Auction? execute()
     {
-        return new Auction
-        {
-           
-        };
+        var repository = new AuctionProjectDbContext();
+
+        var today = DateTime.Now;
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
     }
 }
