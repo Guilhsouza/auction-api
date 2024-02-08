8765acdef4cb3d40247aa75d75c69399258ccb30
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-07 21:47:19 -0300

    feat(CreateOffer.cs) adicionando endpoint de usuário criar uma oferta logado e com valor fixo

---
+++ src/Auction.API/useCases/Offers/CreateOffer/CreateOffer.cs
@@ -0,0 +1,34 @@
+﻿using AuctionProject.API.Communication.Requests;
+using AuctionProject.API.Entities;
+using AuctionProject.API.Repositories;
+using AuctionProject.API.Services;
+
+namespace AuctionProject.API.useCases.Offers.CreateOffer;
+
+public class CreateOffer
+{
+    private readonly LoggedUser _loggedUser;
+
+    public CreateOffer(LoggedUser loggedUser) => _loggedUser = loggedUser; 
+
+    public int Execute(int ItemId, RequestCreateOfferJSON request)
+    {
+        var repository = new AuctionProjectDbContext();
+
+        var user = _loggedUser.User();
+
+        var offer = new Offer
+        {
+            CreatedOn = DateTime.Now,
+            ItemId = ItemId,
+            Price = request.Price,
+            UserId = user.Id
+        };
+
+        repository.Offers.Add(offer);
+        
+        repository.SaveChanges();
+
+        return offer.Id;
+    }
+}
