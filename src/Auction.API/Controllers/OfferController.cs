d3039c255dfdb2067e3ce437ed45dbc9735448fa
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-07 21:53:38 -0300

    feat(OfferController) adicionando controlador para criação de ofertas

---
+++ src/Auction.API/Controllers/OfferController.cs
@@ -0,0 +1,22 @@
+﻿using AuctionProject.API.Communication.Requests;
+using AuctionProject.API.Filters;
+using AuctionProject.API.useCases.Offers.CreateOffer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionProject.API.Controllers;
+
+public class OfferController : AuctionBaseController
+{
+    [HttpPost]
+    [Route("{itemId}")]
+    [ServiceFilter(typeof(AuthenticationUserAttribute))]
+    public IActionResult CreateOffer(
+        [FromRoute] int itemId,
+        [FromBody] RequestCreateOfferJSON request,
+        [FromServices] CreateOffer useCase)
+    {
+        var id = useCase.Execute(itemId, request);
+
+        return Created(string.Empty, id);
+    }
+}
