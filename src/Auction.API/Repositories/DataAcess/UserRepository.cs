117f88c2bde4e43f36c0e036a7d66ad550c05547
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-09 11:48:49 -0300

    feat(Repositories) adicionando repositorios para trabalhar com Interfaces em Auction, User e Offer

---
+++ src/Auction.API/Repositories/DataAcess/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using AuctionProject.API.Contracts;
+using AuctionProject.API.Entities;
+
+namespace AuctionProject.API.Repositories.DataAcess;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AuctionProjectDbContext _dbContext;
+
+    public UserRepository(AuctionProjectDbContext dbContext) => _dbContext = dbContext;
+
+    public bool ExistUserWithEmail(string email)
+    {
+        return _dbContext.Users.Any(Users => Users.Email.Equals(email));
+    }
+
+    public User GetUserByEmail(string email) => _dbContext.Users.First(user => user.Email.Equals(email));
+}
