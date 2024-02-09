047ab46aa17b788847f0c346699a28f2f48e6aa7
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-09 11:46:29 -0300

    feat(Contracts) adicionando interfaces para Auction, User e Offer

---
+++ src/Auction.API/Contracts/IAuctionRepository.cs
@@ -0,0 +1,8 @@
+﻿using AuctionProject.API.Entities;
+
+namespace AuctionProject.API.Contracts;
+
+public interface IAuctionRepository
+{
+    Auction GetCurrent();
+}
---
+++ src/Auction.API/Contracts/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using AuctionProject.API.Entities;
+
+namespace AuctionProject.API.Contracts;
+
+public interface IUserRepository
+{
+    bool ExistUserWithEmail(string email);
+    User GetUserByEmail(string email);
+}
