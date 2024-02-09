dac0b235c90430e822de84f2afb1cbc50d1f932e
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-09 16:27:08 -0300

    feat(CreateOfferTest.cs) adicionando testes para criação de ofertas

---
+++ tests/UseCases.Test/Offers/CreateOfferTest.cs
@@ -0,0 +1,34 @@
+﻿using AuctionProject.API.Communication.Requests;
+using AuctionProject.API.Contracts;
+using AuctionProject.API.Entities;
+using AuctionProject.API.Repositories.DataAcess;
+using AuctionProject.API.Services;
+using AuctionProject.API.useCases.Offers.CreateOffer;
+using Bogus;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace UseCases.Test.Offers;
+public class CreateOfferTest
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void Sucess(int itemId)
+    {
+        var request = new Faker<RequestCreateOfferJSON>()    
+            .RuleFor(request => request.Price, f => f.Random.Decimal(1, 100)).Generate();
+
+        var offerRepository = new Mock<IOfferRepository>();
+        var loggedUser = new Mock<ILoggedUser>();
+        loggedUser.Setup(i => i.User()).Returns(new User());
+
+        var useCase = new CreateOffer(loggedUser.Object ,offerRepository.Object);
+
+        var act = () => useCase.Execute(itemId, request);
+
+        act.Should().NotThrow();
+    }
+}
