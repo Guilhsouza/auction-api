8d8c209939672656f9f125efe63089936bf868d9
Guilherme Souza <gui.edu0380@gmail.com>
2024-02-09 11:50:50 -0300

    feat(GetCurrentAuction.cs) adicionando configuração para inserir injeção de dependencias

--- src/Auction.API/useCases/Auctions/GetCurrent/GetCurrentAuction.cs
+++ src/Auction.API/useCases/Auctions/GetCurrent/GetCurrentAuction.cs
@@ -1,20 +1,19 @@
-﻿using AuctionProject.API.Entities;
+﻿using AuctionProject.API.Contracts;
+using AuctionProject.API.Entities;
 using AuctionProject.API.Repositories;
+using AuctionProject.API.Repositories.NewFolder;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionProject.API.useCases.Auctions.GetCurrent;
 
 public class GetCurrentAuction
 {
+    private readonly IAuctionRepository _repository;
+
+    public GetCurrentAuction(IAuctionRepository repository) => _repository = repository;
+
     public Auction? execute()
     {
-        var repository = new AuctionProjectDbContext();
-
-        var today = new DateTime(2024, 01, 25);
-
-        return repository
-            .Auctions
-            .Include(auction => auction.Items)
-            .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
+        return _repository.GetCurrent();
     }
 }
